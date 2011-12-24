e3facf3d1d68accc653120c2acb93f3c6838fec0
Zeb Palmer <zeb@zebpalmer.com>
2011-12-23 20:49:01 -0700

    initial tests

--- nagparser/test/test_nag_parser.py
+++ nagparser/test/test_nag_parser.py
@@ -1,22 +1,23 @@
 # Some basic Nose tests....
-
+from nose.tools import *
 import nagparser
 
-basedir = './'
-importantservicegroups = None
-files = [basedir + 'test_objects.cache', basedir + 'test_status.dat']
-config = {'importantservicegroups': importantservicegroups, 'files': files}
 
 
-
-def test_nag_objcreation(config):
-    '''Just create the nag object, fail on any exception'''
-    nag = nagparser.parse(config)
-    
-    
-
-def test_lastupdated_matches_testdata(config):
-    '''Test to ensure the timestamp matches expected given test data'''
-    nag = nagparser.parse(config)
-    lastupdated = '2011-10-31 12:08:22'
-    asert (nag.lastupdated == lastupdated)
+class Test_nagparser():
+    def setup(self):
+        basedir = './'
+        importantservicegroups = None
+        files = [basedir + 'test_objects.cache', basedir + 'test_status.dat']
+        self.config = {'importantservicegroups': importantservicegroups, 'files': files}    
+        self.lastupdated = '2011-10-31 12:08:22'
+        
+        def test_nag_objcreation():
+            '''Just create the nag object, fail on any exception'''
+            nag = nagparser.parse(self.config)
+            
+        
+        def test_lastupdated_matches_testdata():
+            '''Test to ensure the timestamp matches expected given test data'''
+            nag = nagparser.parse(config)
+            asert (nag.lastupdated == self.lastupdated)
