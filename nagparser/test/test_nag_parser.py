19dfc7ae7576bb7cc064c4c0859a8d0a66157c26
Zeb Palmer <zeb@zebpalmer.com>
2011-12-23 20:52:39 -0700

    missing self(s)

--- nagparser/test/test_nag_parser.py
+++ nagparser/test/test_nag_parser.py
@@ -12,12 +12,12 @@ class test_nagparser():
         self.config = {'importantservicegroups': importantservicegroups, 'files': files}    
         self.lastupdated = '2011-10-31 12:08:22'
         
-    def test_nag_objcreation():
+    def test_nag_objcreation(self):
         '''Just create the nag object, fail on any exception'''
         nag = nagparser.parse(self.config)
         
     
-    def test_lastupdated_matches_testdata():
+    def test_lastupdated_matches_testdata(self):
         '''Test to ensure the timestamp matches expected given test data'''
         nag = nagparser.parse(config)
         asert (nag.lastupdated == self.lastupdated)
