4107c9f7f3ca27f29807f13db224cde0b04c6135
Zeb Palmer <zeb@zebpalmer.com>
2011-12-23 20:51:08 -0700

    indent level fix

--- nagparser/test/test_nag_parser.py
+++ nagparser/test/test_nag_parser.py
@@ -4,7 +4,7 @@ import nagparser
 
 
 
-class Test_nagparser():
+class test_nagparser():
     def setup(self):
         basedir = './'
         importantservicegroups = None
@@ -12,12 +12,12 @@ class Test_nagparser():
         self.config = {'importantservicegroups': importantservicegroups, 'files': files}    
         self.lastupdated = '2011-10-31 12:08:22'
         
-        def test_nag_objcreation():
-            '''Just create the nag object, fail on any exception'''
-            nag = nagparser.parse(self.config)
-            
+    def test_nag_objcreation():
+        '''Just create the nag object, fail on any exception'''
+        nag = nagparser.parse(self.config)
         
-        def test_lastupdated_matches_testdata():
-            '''Test to ensure the timestamp matches expected given test data'''
-            nag = nagparser.parse(config)
-            asert (nag.lastupdated == self.lastupdated)
+    
+    def test_lastupdated_matches_testdata():
+        '''Test to ensure the timestamp matches expected given test data'''
+        nag = nagparser.parse(config)
+        asert (nag.lastupdated == self.lastupdated)
