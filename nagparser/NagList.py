2ae619aa0c2e4fb672ab66a666cdc723400cb0d8
Matthew Kennedy <mattkenn4545@gmail.com>
2012-01-30 13:49:30 -0700

    Re organized the logic to be more performant in common usage cases. Converted everything to list comprehentions.

--- nagparser/NagList.py
+++ nagparser/NagList.py
@@ -1,24 +1,23 @@
 class NagList(list):
     def __getattr__(self, name):
-        _name = str(name).replace('_', '-')
-        obj = filter(lambda x: x.name == _name, self)
+
         
+        if name == 'first':
+            if self:
+                return self[0]
+            else:
+                return None
+        
+        if name == 'names':
+            return [x.name for x in self]
+        
+        #_name = str(name).replace('_', '-')
+        #obj = filter(lambda x: x.name == name, self)
+        obj = [x for x in self if x.name == name]
         if obj:
             if len(obj) == 1:
                 return obj[0]
             else:
                 raise AttributeError('Multiple instances found')
-            
-        elif name == 'names':
-            results = []
-            for obj in self:
-                results.append(obj.name)
-                
-            return results
-        elif name == 'first':
-            if self:
-                return self[0]
-            else:
-                return None
-        else:
-            raise AttributeError
+
+        raise AttributeError
